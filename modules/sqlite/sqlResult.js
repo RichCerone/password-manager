51c698252196fe5f6b95ddcaddbc06bc2d043a80
Rich Cerone <rcerone@live.com>
2021-05-06 17:11:41 -0400

    Made minor improvements to SQL service. Added ability to verify login and password hashes.

--- modules/sqlite/sqlResult.js
+++ modules/sqlite/sqlResult.js
@@ -4,17 +4,20 @@
  */
 class SqlResult {
     /**
+     * Represents the result from using the SqliteService.
      * 
      * @constructs SqlResult
      * @param {string} message Any application specific message to return.
      * @param {boolean} successful true if the statement executed successfully--false if not.
      * @param {boolean} uniqueException true if the entery already exists--false if not.
-     * @param {*} sqlMsg SQLite specific message for logging purposes.
+     * @param {*} result The results of the query (if any)
+     * @param {string} sqlMsg SQLite specific message for logging purposes.
      */
-    constructor(message = '', successful = false, uniqueException = false, sqlMsg = '') {
+    constructor(message = null, successful = false, uniqueException = false, result = null, sqlMsg = '') {
         this.message = message;
         this.successful = successful;
         this.uniqueException = uniqueException;
+        this.result = result;
         this.sqlMsg = sqlMsg
     }
 }
--- renderers/index/signup.js
+++ renderers/index/signup.js
@@ -2,8 +2,6 @@
  * This helps redirect to render the signup page.
  ****************************************************/
 
-const { ipcRenderer } = require('electron');
-
 let signupButton = document.getElementById('signup');
 signupButton.addEventListener('click', () =>
 {
